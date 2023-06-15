60a54e6935b673a2003676e19756b75b0f4b9327
mohammedaladhary <caladhary@gmail.com>
2023-06-15 09:55:11 +0400

    sort and search array

--- Program.cs
+++ Program.cs
@@ -2,15 +2,16 @@
 {
     static int[] sortArray(int[] num)
     {
-        int temp = 0;
-        for (int i = 1; i < num.Length; i++)
+        for (int i = 0; i < num.Length; i++)
         {
-            for (int j = 1; j < num.Length -1; j++)
-            if (num[j] > num[j + 1])
+            for (int j = i + 1; j < num.Length; j++)
             {
-                temp = num[j + 1];
-                    num[j+1] = num[j];
+                if (num[i] > num[j])
+                {
+                    int temp = num[i];
+                    num[i] = num[j];
                     num[j] = temp;
+                }
             }
         }
         return num;
@@ -18,7 +19,7 @@
 
     static int searchArray(int[] num, int schNum)
     {
-        for (int i = 1; i < num.Length; i++)
+        for (int i = 0; i < num.Length; i++)
         {
             if (schNum == num[i])
             {
@@ -28,17 +29,21 @@
         return -1;
     }
 
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
         int[] arr = new int[8] { 4, 7, 3, 5, 1, 10, 90, 8 };
-     
-        Console.WriteLine("The sorted array is: " + sortArray(arr));
-        //[1,3,4,5,7,8,10,90] sorted array
+        int[] anArray = sortArray(arr);
+        Console.WriteLine("The sorted array is: ");
+        for (int i = 0; i < anArray.Length; i++)
+        {
+            Console.Write(anArray[i] + " ");
+        }
+        Console.WriteLine();
 
-        Console.WriteLine("Searched array: " + searchArray(arr , 7));
+        Console.WriteLine("Searched array: " + searchArray(arr , 10));
         ////5th place - index in the array
 
-        Console.WriteLine("Searched array: " + searchArray(arr, 20));
+        Console.WriteLine("Sea+rched array: " + searchArray(arr, 20));
         ////-1 - not in the array
 
     }
